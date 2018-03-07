88d7944fe9bd718c5ccde84fd4e50f0b3b575daa
Twan van Laarhoven <twanvl@gmail.com>
2018-03-08 00:33:47 +0100

    Add LibraryType enum

--- src/main/java/hybridcharacter/patches/Enums.java
+++ src/main/java/hybridcharacter/patches/Enums.java
@@ -3,10 +3,14 @@ package hybridcharacter.patches;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 
 public class Enums {
     @SpireEnum
     public static AbstractCard.CardColor HYBRID_COLOR;
+
+	  @SpireEnum
+	  public static CardLibrary.LibraryType HYBRID_COLOR;
 
     @SpireEnum
     public static AbstractPlayer.PlayerClass HYBRID;
